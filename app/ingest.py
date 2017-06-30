0182d289bb26205c98719f6330a22f680ade6b71
Small-Star <small_star@gmx.com>
2017-06-30 04:18:15 -0400

    Modified db schema, improved validation

--- app/ingest.py
+++ app/ingest.py
@@ -4,8 +4,9 @@ import re, datetime, os
 from app import app, db
 from app.models import Mood
 
-MOOD_S = ['L','U','M','N']
+from collections import namedtuple
 
+Mood_Tup = namedtuple('Mood_Tup', 'date a_l a_u a_s v_l v_u v_s')
 def ingest_mood(date=""):
     '''Reads data from MOOD_FILE and inputs it into db'''
     Mood_Parser = Mood_HTML_Parser()
@@ -16,20 +17,55 @@ def ingest_mood(date=""):
     t_a = Mood_Parser.get_moods()
 
     #Validate
-    t_a_ = [t for t in t_a if (type(t[0])==datetime.date and (t[1] > 0) and (t[1] < 10) and (t[2] > 0) and (t[2] < 10) and (t[3] in MOOD_S) == True and (t[4] > 0) and (t[4] < 10) and (t[5] > 0) and (t[5] < 10) and (t[6] in MOOD_S) == True)]
-    if len(t_a_) != len(t_a):
-        print("Validation error")
-    
+    MOOD_S = ['L','U','M','N']
+
+    def val_mood_params(mt):
+        try:
+            assert type(mt.date)==datetime.date and mt.date >= datetime.date(2015,11,30), "date"
+            assert mt.a_l > 0 and mt.a_l < 10 and mt.a_u > 0 and mt.a_u < 10 and mt.v_l > 0 and mt.v_l < 10 and mt.v_u > 0 and mt.v_u < 10, "numeric value"
+            assert mt.a_s in MOOD_S and mt.v_s in MOOD_S, "s value"
+            return mt
+        except Exception as e:
+            print("Validation error in mood ingest for date " + str(mt.date) + ": " + e.args[0])
+            return Mood_Tup(date=datetime.date(2100,1,1), a_l=5,a_u=5,a_s='N',v_l=5,v_u=5,v_s='N')
+
+    t_a_ = [val_mood_params(t) for t in t_a]
+
     #Try and add entry to db
     for _ in t_a_:
-        if Mood.query.get(_[0]) == None:
-            db.session.add(Mood(_[0],_[1],_[2],_[3],_[4],_[5],_[6]))
-            print("Adding: " + str(_[0]))
+        if Mood.query.get(_.date) == None:
+            db.session.add(Mood(_.date,_.a_l,_.a_u,_.a_s,_.v_l,_.v_u,_.v_s))
+            #print("Adding: " + str(_.date))
         else:
-            print("Duplicate: " + str(_[0]))
+            print("Duplicate: " + str(_.date))
 
     db.session.commit()
     print("Mood Ingest complete")
+
+
+
+# def ingest_diet(date=""):
+#     '''Reads data from DIET_FILE and inputs it into db'''
+#     Diet_Parser = Diet_HTML_Parser()
+#     f_name = open(os.path.join(app.config["BASE_FDIR"], app.config["DIET_FILE"]), 'r')
+#
+#     #Read data
+#     Diet_Parser.feed(f_name.read())
+#     t_a = Diet_Parser.get_diet_vals()
+#
+#     #Validate
+#     #***TODO: validate
+#
+#     #Try and add entry to db
+#     for _ in t_a_:
+#         if QS_Params.query.get(_[0]) == None:
+#             db.session.add(QS_Params(_[0])
+#             print("Adding QS Param: " + str(_[0]))
+#         else:
+#             print("Duplicate QS Param: " + str(_[0]))
+#
+#     db.session.commit()
+#     print("Diet Ingest complete")
 
 class Mood_HTML_Parser(HTMLParser):
     ''' Input an xml file, return a list of tuples to add to the db'''
@@ -37,15 +73,65 @@ class Mood_HTML_Parser(HTMLParser):
         HTMLParser.__init__(self)
 
         self.to_add = [] #List of tuples that will be returned for adding to the db
-
         self.date_str_re = re.compile(r'\d\d\/\d\d\/\d\d')
 
     def handle_data(self, data):
         data_ = data.strip()    #Drop whitespace
 
         if re.match(self.date_str_re,data_) != None:
-            date = datetime.datetime.strptime(data_[:10],"%m/%d/%Y").date()
-            self.to_add.append((date,int(data_[14]),int(data_[16]),data_[17],int(data_[20]),int(data_[22]),data_[23]))
+            self.to_add.append(Mood_Tup(date = datetime.datetime.strptime(data_[:10],"%m/%d/%Y").date(), a_l = int(data_[14]), a_u = int(data_[16]), a_s = data_[17], v_l = int(data_[20]), v_u = int(data_[22]), v_s = data_[23]))
 
     def get_moods(self):
         return self.to_add
+#
+# class Diet_HTML_Parser(HTMLParser):
+#     ''' Input an xml file, return a list of tuples to add to the db'''
+#     def __init__(self):
+#         HTMLParser.__init__(self)
+#
+#         self.to_add = [] #List of tuples that will be returned for adding to the db
+#         self.calorie_intake = []
+#         self.protein_intake = []
+#         self.tdee = []
+#         self.day_nodes = collections.OrderedDict()
+#
+#         self.date_re = re.compile(r'- \d\d\/\d\d\/\d\d\d\d')      #ex: '03/22/2014'
+#         self.calorie_intake_re = re.compile(r'\s*Intake: ')
+#         self.protein_intake_re = re.compile(r'\s*Protein: ')
+#         self.tdee_re = re.compile(r'\s*Est. TDEE: ')
+#         self.fic_re = re.compile(r'\S+') #Note, this pattern is used on the BACKWARDS TDEE string
+#
+#     def handle_data(self, data):
+#         #Line by line parsing of input data
+#         if re.match(self.date_re,data) != None:
+#             day_str_list = re.split('/',data[2:-1]) #Remove cruft from date
+#             self.current_date = datetime.date(int(day_str_list[2]),int(day_str_list[0]),int(day_str_list[1]))
+#             new_day_node = day_node(self.current_date)
+#             self.day_nodes[self.current_date.toordinal()] = new_day_node
+#             self.day_nodes[new_day_node.get_date().toordinal()] = new_day_node #Add new date to dict: key is the ordinal date
+#         elif re.match(self.calorie_intake_re,data) != None:
+#             node = self.day_nodes.get(self.current_date.toordinal(),day_node(datetime.date(1000,01,01))) #01/01/1000 denotes an error
+#             cal_int = int(re.split(self.calorie_intake_re,re.split("kcal",data)[0])[1])#Match pattern, split away extraneous stuff
+#             node.set_calorie_intake(cal_int)
+#             self.calorie_intake.append(cal_int)
+#         elif re.match(self.protein_intake_re,data) != None:
+#             node = self.day_nodes.get(self.current_date.toordinal(),day_node(datetime.date(1000,01,01))) #01/01/1000 denotes an error
+#             pro_int = int(re.split(self.protein_intake_re,re.split("g",data)[0])[1])#Match pattern, split away extraneous stuff
+#             node.set_protein_intake(pro_int)
+#             self.protein_intake.append(pro_int)
+#         elif re.match(self.tdee_re,data) != None:
+#             node = self.day_nodes.get(self.current_date.toordinal(),day_node(datetime.date(1000,01,01))) #01/01/1000 denotes an error
+#             tdee_ = int(re.split(self.tdee_re,re.split("kcal",data)[0])[1])#Match pattern, split away extraneous stuff
+#             node.set_tdee(tdee_)
+#             node.set_fic(re.search(self.fic_re,data[-2::]).group())     #Pulls out the endmost non-whitespace character(s)
+#             self.tdee.append(tdee_)
+#
+#     def get_calorie_intake(self):
+#         return self.calorie_intake
+#     def get_protein_intake(self):
+#         return self.protein_intake
+#     def get_tdee(self):
+#         return self.tdee
+#     def get_nodes(self):
+#         print "VD" + str(type(self.day_nodes))
+#         return self.day_nodes
--- app/models.py
+++ app/models.py
@@ -3,9 +3,39 @@ sys.path.insert(0, './app')
 
 from app import db
 
+
+#Main DB
+class QS_Params(db.Model):
+    __tablename__ = 'qs_params'
+
+    date = db.Column(db.Date, index=True, unique=True, primary_key=True)
+
+    #Diet
+    kcal_intake = db.Column(db.Integer)
+    protein_intake = db.Column(db.Integer)
+    protein_intake_error_bar = db.Column(db.Integer)
+    carbs_intake = db.Column(db.Integer)
+    net_carbs_intake = db.Column(db.Integer)
+    tdee_intake = db.Column(db.Integer)
+    tdee_intake_error_bar = db.Column(db.Integer)
+    cycle_phase = db.Column(db.String(length=2))
+    cycle_num = db.Column(db.Integer)
+
+    #Mood
+    mood = db.relationship('Mood', uselist=False, back_populates="qsp", primaryjoin="QS_Params.date == Mood.date")
+
+    def __init__(self, date):
+        self.date = date
+
+    def __repr__(self):
+        return str(self.date) + str(kcal_intake > 0)
+
+
 #Mood
 class Mood(db.Model):
-    cr_date = db.Column(db.Date, index=True, unique=True, primary_key=True)
+    __tablename__ = 'mood'
+    date = db.Column(db.Date, db.ForeignKey('qs_params.date'), index=True, unique=True, primary_key=True)
+    qsp = db.relationship("QS_Params", back_populates="mood")
     a_l = db.Column(db.Integer)
     a_u = db.Column(db.Integer)
     a_s = db.Column(db.String(length=1))
@@ -16,8 +46,8 @@ class Mood(db.Model):
     a_be = db.Column(db.Integer)
     v_be = db.Column(db.Integer)
 
-    def __init__(self, cr_date, a_l, a_u, a_s, v_l, v_u, v_s):
-        self.cr_date = cr_date
+    def __init__(self, date, a_l, a_u, a_s, v_l, v_u, v_s):
+        self.date = date
         self.a_l = a_l
         self.a_u = a_u
         self.a_s = a_s
@@ -32,15 +62,13 @@ class Mood(db.Model):
     def lobe(self,l,u,s):
         '''Return single value best estimate'''
         if s == 'U':
-            return float(u - (u - l)*(5/6))
-        if s == 'L':
             return float(u - (u - l)*(1/6))
+        if s == 'L':
+            return float(u - (u - l)*(5/6))
         if s == 'M' or s == 'N':
             return float(u - (u - l)*.5)
         else:
             return 5
 
-
-
     def __repr__(self):
-        return str(self.cr_date) + ":" + str(self.a_l) + ":" + str(self.a_u) + ":" + self.a_s + ":" + str(self.v_l) + ":" + str(self.v_u) + ":" + self.v_s
+        return str(self.date) + ":" + str(self.a_l) + ":" + str(self.a_u) + ":" + self.a_s + ":" + str(self.v_l) + ":" + str(self.v_u) + ":" + self.v_s

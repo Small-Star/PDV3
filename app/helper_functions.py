9af9f366451a59dae0af183a8573a02e7d5d6a0a
Small-Star <small_star@gmx.com>
2018-02-11 15:24:29 -0500

    Graphing bugfixes

--- app/helper_functions.py
+++ app/helper_functions.py
@@ -7,19 +7,34 @@ var r = 0
 
 cn = w.column_names
 for (h = 0; h < cn.length; h++) {
-    if (cn[h] != 'date'){
-        working = wd[cn[h]]
-        static = sd[cn[h]]
+
+    working = wd[cn[h]]
+    static = sd[cn[h]]
+
+    if ((cn[h] == 'date') || (cn[h] == 'date_str')){
+        working = Object.assign({},static)
+        continue;
     }
+
+
+
     for (i = 0;  i < working.length; i++){
         working[i] = 0
         r = Math.min(ma,i)
         for (j = 0; j < r; j++){
             working[i] += static[i-j]
         }
-        working[i] = working[i]/r
+        working[i] = (working[i]/r).toFixed(0)
     }
 }
 w.change.emit();
+
+""")
+
+STATS_CODE = ("""
+
+var div = d;
+div.text = String(cb_obj.end)
+div.change.emit()
 
 """)

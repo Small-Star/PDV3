6b41a87f63b190b653f015fc91061fdd9bdf2a8c
Small-Star <small_star@gmx.com>
2018-11-29 05:11:47 -0500

    Added flag to control database ingest

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+
 import logging
 
 
@@ -15,17 +16,31 @@ db = SQLAlchemy(app)
 
 
 from app import views, models, ingest
+from app.models import QS_Params
+
+ingest_flag = "FORCE"
+
+if ingest_flag == "FORCE":
+    db.drop_all()
+
 db.create_all()
-logging.info("Ingesting mood data...")
-#ingest.ingest_mood()
-logging.info("Ingesting diet data...")
-#ingest.ingest_diet()
-logging.info("Ingesting heart rate data...")
-#ingest.ingest_rhr()
-logging.info("Ingesting sleep data...")
-#ingest.ingest_sleep()
-logging.info("Ingesting blood data...")
-#ingest.ingest_blood()
-logging.info("Ingesting LIFTS...")
-#ingest.ingest_weightlifting()
-logging.info("-------------")
+num_rec = db.session.query(QS_Params).count()
+logging.info("Loading database with %s records", num_rec)
+
+if ingest_flag == "MERGE" or ingest_flag == "FORCE":
+    logging.info("Ingesting mood data...")
+    ingest.ingest_mood()
+    logging.info("Ingesting diet data...")
+    ingest.ingest_diet()
+    logging.info("Ingesting heart rate data...")
+    ingest.ingest_rhr()
+    logging.info("Ingesting sleep data...")
+    ingest.ingest_sleep()
+    logging.info("Ingesting blood data...")
+    ingest.ingest_blood()
+    logging.info("Ingesting LIFTS...")
+    ingest.ingest_weightlifting()
+    logging.info("-------------")
+
+num_rec = db.session.query(QS_Params).count()
+logging.info("Database has %s records", num_rec)
